4b3e92dfad9c2e16a66b8269d8e312886df647c1
marcin <marcin4www@gmail.com>
2024-09-13 01:10:16 +0200

    komit2

---
+++ ajax.php
@@ -0,0 +1,7 @@
+<?php
+	if ($_POST['dane_1'] == '111') {
+		print ('dane_1 = 111');
+	else {
+		print ('else');
+	}
+?>
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 <script>
-
+function relogin() {
+	$.ajax({
+		url: 'ajax.php',
+		methodType: 'JSON',
+		method: 'POST',
+		data: {
+			dane_1: "111",
+			dane_1: "222",
+		},
+		success: function(json) {
+			console.log(json);
+		}
+	});	
+}
 </script>
